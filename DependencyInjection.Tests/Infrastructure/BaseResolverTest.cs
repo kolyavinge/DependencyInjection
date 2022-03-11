af71b497404263caeec6df4798e615bb24e23455
Nikolay Vinge <vn2756@gmail.com>
2022-03-11 23:53:43 +0400

    Resolvers refactoring

--- DependencyInjection.Tests/Infrastructure/BaseResolverTest.cs
+++ DependencyInjection.Tests/Infrastructure/BaseResolverTest.cs
@@ -4,7 +4,7 @@ using Moq;
 
 namespace DependencyInjection.Tests.Infrastructure
 {
-    internal class BaseResolverTest<TResolver> where TResolver : Resolver, new()
+    internal class BaseResolverTest<TResolver> : IResolvingProvider where TResolver : Resolver, new()
     {
         protected BindingContainer _bindingContainer;
         protected Mock<IInvokationContext> _invokationContextMock;
@@ -14,11 +14,12 @@ namespace DependencyInjection.Tests.Infrastructure
         {
             _bindingContainer = new BindingContainer();
             _invokationContextMock = new Mock<IInvokationContext>();
+            _invokationContextMock.Setup(x => x.ResolvingProvider).Returns(this);
             _resolver = new TResolver();
             _resolver.Init(_bindingContainer, _invokationContextMock.Object);
         }
 
-        protected T Resolve<T>()
+        public T Resolve<T>()
         {
             return (T)_resolver.Resolve(typeof(T));
         }
